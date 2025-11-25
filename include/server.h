a1d34c6f4c0c279656fd5b99e58811fc54ad0743
vmoskaev <vmoskaev@srv.rbt>
2025-11-25 15:51:13 +0300

    На стороне сервера реализован механизм обработки отключения клиента. Код отправки сообщения клиенту и вывода в консоль результата отправки сделан в виде отдельных методов send и printSendResult класса сервера.

--- include/server.h
+++ include/server.h
@@ -11,12 +11,11 @@
 static const char *const ipServer = "127.0.0.1";
 
 static std::vector< std::shared_ptr< struct sockaddr_in > > udpClients;
-static std::vector< std::shared_ptr< struct sockaddr_in > > tcpClients;
-static int countAllTcpClients{};
-static int countConnectedTcpClients{};
+static std::vector< int > tcpClientSockets;
+static int countAllClients{};
 
 /**
- * Базовый класс сервера
+ * Класс сервера TCP, UDP
  */
 class Server : public BaseClientServer
 {
@@ -29,7 +28,6 @@ public:
       UP, // Работает
       INIT_ERROR, // Ошибка создания сокета
       BIND_ERROR, // Ошибка подключения сокета к серверу
-      KEEP_ALIVE_ERROR,
       LISTEN_ERROR, // Ошибка включения прослушивания сокета
       CLOSE_ERROR, // Ошибка закрытия сокета
       CLOSE // Выключен
@@ -64,23 +62,66 @@ public:
    static ProcessState process( int socket, uint16_t port, bool *isStop = nullptr,
                                 SocketType typeServer = SocketType::TCP );
    
-   static ProcessState processMessage( char* msg, ssize_t msgSize, int socket,
-                               const std::string& logMsg, SocketType typeServer,
-                                       sockaddr_in *addr = nullptr, bool* isStop = nullptr );
-   
    /**
     * Останавливает сервер
     * @param socket сокет
+    * @param typeServer тип сервера
+    * @param isStop признак останова обработки сообщений
     * @return статус сервера
     */
-   static Status stop( int socket, bool *isStop );
-   
+   static Status stop( int socket, SocketType typeServer, bool *isStop );
+
+   /**
+    * Формирует сообщение лога
+    * @param port порт
+    * @param typeServer тип сервера
+    * @return сообщение лога
+    */
    static std::string serverMessage( uint16_t port, SocketType typeServer );
 
 private:
-   // Статус сервера
-   Status m_status = Status::CLOSE;
-   
+    // Статус сервера
+    Status m_status = Status::CLOSE;
+
+    /**
+     * Обрабатывает сообщение от клиента
+     * @param msg сообщение
+     * @param msgSize размер сообщения
+     * @param socket сокет
+     * @param logMsg сообщение лога
+     * @param typeServer тип сервера: TCP, UDP
+     * @param addr адрес сервера
+     * @param isStop признак останова обработки сообщений
+     * @return статус обработки сообщения
+     */
+    static ProcessState processMessage( char* msg, ssize_t msgSize, int socket,
+                                        const std::string& logMsg, SocketType typeServer,
+                                        sockaddr_in *addr = nullptr, bool* isStop = nullptr );
+
+    /**
+     * Выводит результат отправки в консоль
+     * @param result результат отправки сообщения
+     * @param logMsg сообщение лога
+     * @param msg сообщение клиенту
+     * @param sendSuccess сообщение об успешной отправке
+     * @param sendFail сообщение о неудачной отправке
+     */
+    static void printSendResult( ssize_t result, const std::string &logMsg,
+                                 const char* msg, const char* sendSuccess,
+                                 const char* sendFail );
+
+    /**
+     * Отправляет сообщение клиенту
+     * @param socket сокет
+     * @param logMsg сообщение лога
+     * @param msg сообщение для отправки
+     * @param typeServer тип сервера
+     * @param addr адрес клиента
+     * @param sendSuccess сообщение об успешной отправке
+     * @param sendFail сообщение о неудачной отправке
+     */
+    static void send( int socket, const std::string& logMsg, const std::string& msg, SocketType typeServer,
+               sockaddr_in* addr, const char* sendSuccess, const char* sendFail );
 };
 
 #endif //TCPUDPSERVER_SERVER_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,35 @@
 #include "server.h"
 
 int main() {
-    int tcpPort = 8080;
-    bool isTcpServerStop = false;
-    Server tcpServer( tcpPort );
-    auto tcpServerStatus = tcpServer.start( Server::SocketType::TCP );
-    std::future< Server::ProcessState > tcpProcessResult{};
-    if( tcpServerStatus == Server::Status::UP ) {
-       int  tcpServerSocket  = tcpServer.currentSocket( );
-       tcpProcessResult = std::async( Server::process, tcpServerSocket,
-                                           tcpPort, &isTcpServerStop,
-                                           Server::SocketType::TCP );
-    }
-    
-    int    udpPort          = 8081;
-    bool   isUdpServerStop  = false;
-    Server udpServer( udpPort );
-    auto   udpServerStatus  = udpServer.start( Server::SocketType::UDP );
-    std::future< Server::ProcessState > udpProcessResult{ };
-    if( udpServerStatus == Server::Status::UP ) {
-       int  udpServerSocket  = udpServer.currentSocket( );
-       udpProcessResult = std::async( Server::process, udpServerSocket,
-                                           udpPort, &isUdpServerStop, Server::SocketType::UDP );
-    }
+   int tcpPort = 8080;
+   bool isTcpServerStop = false;
+   // Создаем TCP сервер
+   Server tcpServer( tcpPort );
+   // Запускаем его
+   auto tcpServerStatus = tcpServer.start( Server::SocketType::TCP );
+   std::future< Server::ProcessState > tcpProcessResult{};
+   // Если сервер запустился, то запускаем поток обработки сообщений от клиентов
+   if( tcpServerStatus == Server::Status::UP ) {
+      int  tcpServerSocket  = tcpServer.currentSocket( );
+      tcpProcessResult = std::async( Server::process, tcpServerSocket,
+                                     tcpPort, &isTcpServerStop,
+                                     Server::SocketType::TCP );
+   }
    
+   int    udpPort          = 8081;
+   bool   isUdpServerStop  = false;
+   // Создаем UDP сервер
+   Server udpServer( udpPort );
+   // Запускаем его
+   auto   udpServerStatus  = udpServer.start( Server::SocketType::UDP );
+   std::future< Server::ProcessState > udpProcessResult{ };
+   // Если сервер запустился, то запускаем поток обработки сообщений от клиентов
+   if( udpServerStatus == Server::Status::UP ) {
+      int  udpServerSocket  = udpServer.currentSocket( );
+      udpProcessResult = std::async( Server::process, udpServerSocket,
+                                     udpPort, &isUdpServerStop, Server::SocketType::UDP );
+   }
+   // Ожидаем завершения потоков обраотки сообщений TCP и UDP серверов
    if( tcpServerStatus == Server::Status::UP )
       tcpProcessResult.wait( );
    if( udpServerStatus == Server::Status::UP )
